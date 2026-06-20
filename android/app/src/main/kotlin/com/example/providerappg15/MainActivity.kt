160fd77d673434dd9686813c98c7f4b5bf307da5
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-06-19 19:21:34 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/providerappg15/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.providerappg15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:providerappg15/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
