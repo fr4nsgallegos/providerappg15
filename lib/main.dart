614079e28ba192b4d5bb8486d630948bc2a50439
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-06-19 20:24:28 -0500

    ejemplo con varios providers

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,31 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:providerappg15/pages/counter_page.dart';
 import 'package:providerappg15/pages/home_page.dart';
+import 'package:providerappg15/providers/cart_provider.dart';
 import 'package:providerappg15/providers/counter_provider.dart';
 
+// EJEMPLO CON UN SOLO PROVIDER
+// void main() {
+//   runApp(
+//     ChangeNotifierProvider(
+//       create: (_) => CounterProvider(),
+//       child: MaterialApp(
+//         home: CounterPage(),
+//         debugShowCheckedModeBanner: false,
+//       ),
+//     ),
+//   );
+// }
+
+// EJEMPLO CON VARIOS PROVIDERS
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => CounterProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CounterProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
+
       child: MaterialApp(
         home: CounterPage(),
         debugShowCheckedModeBanner: false,
